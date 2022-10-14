17035cdf6ca19c42ae707f0055eb3f9efa52e469
Alice Moretti <ali.moret88@gmail.com>
2022-10-14 10:33:42 +0300

    Added data model for todo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.css";
+import Todos from "./Components/Todos";
+import todo from "./Models/todo";
 
 function App() {
-  return <div></div>;
+  const todos = [new todo("Learn React"), new todo("rest")];
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
