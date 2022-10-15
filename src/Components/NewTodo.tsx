b6284a221fcae66f050b7a57f1e1863f9b4cf2ea
Alice Moretti <ali.moret88@gmail.com>
2022-10-15 14:19:10 +0300

    Passed state from NewTodo to App

--- src/Components/NewTodo.tsx
+++ src/Components/NewTodo.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { useRef } from "react";
 
-const NewTodo: React.FC = () => {
+//Passing a function as a prop in TS:
+//need to define the arguments type, the type of the returned value (in this case void as we don't need return any value).
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  //we must tell TS that initially the value associated to useRef is empty, otherwise it throws an error
   const textInput = useRef<HTMLInputElement>(null);
   const formSubmission = (e: React.FormEvent) => {
     e.preventDefault();
 
-    const enteredText = textInput.current?.value;
+    const enteredText = textInput.current!.value;
+
+    if (enteredText?.trim().length === 0) {
+      return;
+    }
+
+    props.onAddTodo(enteredText);
   };
   return (
     <form onSubmit={formSubmission}>
