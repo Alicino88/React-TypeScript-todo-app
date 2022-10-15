2e80a107068157c92d07e88c453d017fe8e4e3fd
Alice Moretti <ali.moret88@gmail.com>
2022-10-15 14:19:25 +0300

    Added useState to set the todos

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import React from "react";
 import "./App.css";
 import Todos from "./Components/Todos";
 import todo from "./Models/todo";
+import NewTodo from "./Components/NewTodo";
+import { useState } from "react";
 
 function App() {
-  const todos = [new todo("Learn React"), new todo("Have a hot chocolate")];
+  //initially, todos is an ampty array that will accept objects of the todo type
+  const [todos, setTodos] = useState<todo[]>([]);
+  const addText = (todoText: string) => {
+    const newTodo = new todo(todoText);
+    //to update state based on the previus state the function form is used:
+    setTodos((previousTodos) => {
+      return previousTodos.concat(newTodo);
+    });
+  };
+
   return (
     <div>
+      <NewTodo onAddTodo={addText} />
       <Todos items={todos} />
     </div>
   );
